cdb8fede9aa577c06b2841b8273cc986d56b3ac7
ItsmeLee <arsmagica90@gmail.com>
2020-08-24 17:32:13 +0900

    docs : Add modules
    Add body-parser and cookie-parser

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 import "core-js";
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 const app = express();
 const PORT = 3000;
 
-const handleListening = () =>{
-    console.log(`Server is running: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Server is running: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => {
-    //console.log(req);
-    res.send(`Hi! From Home`);
-}
+
+const handleHome = (req, res) => res.send(`Hi! From Home`);
+
 
 function hadnleProfile(req, res){
     res.send('U are on my profile');
 }
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(morgan("dev"));
 
 app.get('/',handleHome);
 
