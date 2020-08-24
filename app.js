914b78713950eaf678e79da904f44d6267a5fcad
ItsmeLee <arsmagica90@gmail.com>
2020-08-24 12:54:06 +0900

    docs : Install basic modules
    Install basic modules
    for example nodemon,babel,express

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const express = require('express')
-const app = express()
+import "core-js";
+import express from "express";
+const app = express();
 const PORT = 3000;
 
 const handleListening = () =>{
     console.log(`Server is running: http://localhost:${PORT}`);
 }
+
+const handleHome = (req, res) => {
+    //console.log(req);
+    res.send(`Hi! From Home`);
+}
+
+function hadnleProfile(req, res){
+    res.send('U are on my profile');
+}
+
+app.get('/',handleHome);
+
+app.get('/profile',hadnleProfile);
 
 app.listen(PORT,handleListening);
