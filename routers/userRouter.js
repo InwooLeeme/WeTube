632e0aa5b088cb437d31fc7a98f160f0c9addf8f
ItsmeLee <arsmagica90@gmail.com>
2020-08-27 15:26:21 +0900

    테스트 : Join 기능 구현
    join기능을 구현
    search템플릿에 mixin을 사용해서 동영상을 검색했을 시 검색 결과가 나오게 구현
    join기능 : 기능 구현을 위해 join을 getJoin과 postJoin으로 분리
    post된 정보를 받아오기 위해 globalRouter에 post방식으로 route를 추가하였음
    
    password와 확인 password가 다를때는 오류를 출력하고 같을 때는 바로 home 템플릿으로 이동할 수 있는 로직을 추가함

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
 import routes from "../routes";
-import { users, editProfile, changePassword, userDetail } from "../controller/userController";
+import { editProfile, changePassword, userDetail } from "../controller/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);        // /users/users
 userRouter.get(routes.editProfile, editProfile);        //  /users/editProfile
 userRouter.get(routes.changePassword, changePassword);  // /users/change-password
 userRouter.get(routes.userDetail, userDetail);      // /users/userDetail
