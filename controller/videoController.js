18c29ca0aa555d5fba8e89c73a283fe819efcc8e
ItsmeLee <arsmagica90@gmail.com>
2020-09-02 17:53:14 +0900

    Add styling home,Edit Video,Video Detail
    Add the styling home, Edit Video page and Video Detail page
    
    Add many videos to confirm home styling

--- controller/videoController.js
+++ controller/videoController.js
@@ -26,29 +26,32 @@ export const search = async (req, res) =>{
 };
 
 export const getUpload = (req, res) => res.render('upload', {pageTitle : "Upload"});
-export const postUpload = async(req, res) => {
-    const { body : { title, description },
-        file : { path }
+  
+export const postUpload = async (req, res) => {
+    const {
+        body: {
+            title,
+            description
+        },
+        file: {
+            path
+        }
     } = req;
-    const newVideo = await Video.create({
-        fileUrl : path,
-        title,
-        description
-        });
-        console.log(newVideo);
-    // To Do : Upload and save video
+    const newVideo = await Video.create({fileUrl: path, title, description});
     res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = async(req, res) => {
-    const {
-        params : { id }
-    } = req;
-    try{
+export const videoDetail = async (req, res) => {
+    const {params: {
+            id
+        }} = req;
+    try {
         const video = await Video.findById(id);
-        res.render('videoDetail', {pageTitle : `${video.title}`, video });
-    }
-    catch(error){
+        res.render("videoDetail", {
+            pageTitle: video.title,
+            video
+        });
+    } catch (error) {
         res.redirect(routes.home);
     }
 };
