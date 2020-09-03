761d0a442c1f9208a96e09e2f4912ee8ef433a94
ItsmeLee <arsmagica90@gmail.com>
2020-09-03 15:24:37 +0900

    Add the Log In function in WeTube
    Add the Log In function in WeTube
    
    For login Function, use passport-local-mongoose and build the Database Schema
    
    Import User.js to use user model
    
    +Edit the form class name in join.pug

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,11 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) =>{
     res.render('join', {pageTitle : "Join"});
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res, next) => {
     const {
         body:{
             name,
@@ -18,12 +19,17 @@ export const postJoin = (req, res) => {
         res.render('join', {pageTitle : "Join"});
     }
     else {
-        // To Do : register user
-        // To Do : log user in
-        res.redirect(routes.home);
-    }
-    
-}
+        try{
+            const user = await User({name, email});
+            await User.register(user, password);
+            next();
+        }
+        catch(error){
+            console.log(error);
+            res.redirect(routes.home);
+        }
+    }  
+};
 
 export const getLogin = (req, res) => res.render('login', {pageTitle : "Log In"});
 export const postLogin = (req, res) =>{
