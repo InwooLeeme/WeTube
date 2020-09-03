3f1a85cd9a6229ea03bdba27d1cab2adf2cc6c17
ItsmeLee <arsmagica90@gmail.com>
2020-09-03 16:18:00 +0900

    Test Join and Login function
    Test Join and Login function
    
    Added the passport module in userController.js file and change postLogin to middleware
    
    If user success login, user's display screen redirect home if not redirect login page

--- app.js
+++ app.js
@@ -4,26 +4,32 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import passport from "passport";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 import { localMiddleware } from "./middlewares";
+
+import "./passport.js";
+
 const app = express();
 
 app.use(helmet());
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
-app.use("/static",express.static("static"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(passport.initialize());
+app.use(passport.session()); // save session
 
 app.use(localMiddleware);
 
-app.use(routes.home, globalRouter);      // "/"
-app.use(routes.users, userRouter);      // "/users"
-app.use(routes.videos, videoRouter);    // "/videos"
+app.use(routes.home, globalRouter); // "/"
+app.use(routes.users, userRouter); // "/users"
+app.use(routes.videos, videoRouter); // "/videos"
 
 export default app;
--- controller/userController.js
+++ controller/userController.js
@@ -1,44 +1,44 @@
 import routes from "../routes";
 import User from "../models/User";
+import passport from "passport";
 
-export const getJoin = (req, res) =>{
-    res.render('join', {pageTitle : "Join"});
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
 };
 
 export const postJoin = async (req, res, next) => {
-    const {
-        body:{
-            name,
-            email,
-            password,
-            password2
-        }
-    } = req;
-    if(password !== password2){
-        res.status(400);
-        res.render('join', {pageTitle : "Join"});
+  const {
+    body: { name, email, password, password2 },
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    try {
+      const user = await User({ name, email });
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.redirect(routes.home);
     }
-    else {
-        try{
-            const user = await User({name, email});
-            await User.register(user, password);
-            next();
-        }
-        catch(error){
-            console.log(error);
-            res.redirect(routes.home);
-        }
-    }  
+  }
 };
 
-export const getLogin = (req, res) => res.render('login', {pageTitle : "Log In"});
-export const postLogin = (req, res) =>{
-    res.redirect(routes.home);
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
+
+export const postLogin = passport.authenticate("local", {
+  successRedirect: routes.home, //
+  failureRedirect: routes.login, // 비밀 번호가 틀렸을 때
+});
+export const logout = (req, res) => {
+  //To Do : Procss Log Out
+  res.redirect(routes.home);
 };
-export const logout = (req, res) =>{
-    //To Do : Procss Log Out
-    res.redirect(routes.home);
-};
-export const editProfile = (req, res) => res.render('editProfile', {pageTitle : "Edit Profile"});
-export const changePassword = (req, res) => res.render('changePassword', {pageTitle : "Change Password"});
-export const userDetail = (req, res) => res.render('userDetail', {pageTitle : "User Detail"});
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
