4ffff406e2bfb5d82a0708ccb767ee63e207abe9
ItsmeLee <arsmagica90@gmail.com>
2020-09-08 19:14:38 +0900

    Add userProfile styling
    Add the userProfile styling and fixed the bug(can't see video, image)

--- controller/userController.js
+++ controller/userController.js
@@ -88,7 +88,27 @@ export const userDetail = async (req, res) => {
   }
 };
 
-export const editProfile = (req, res) =>
+export const getEditProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const postEditProfile = async (req, res) => {
+  const {
+    body: { name, email },
+    file,
+  } = req;
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      name,
+      email,
+      avatarUrl: file ? file.path : req.user.avatarUrl,
+    });
+    /* console.log(file.filename);
+    console.log(file.originalname); */
+    res.redirect(routes.me);
+  } catch (error) {
+    res.render("editProfile", { pageTitle: "Edit Profile" });
+  }
+};
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,15 +1,18 @@
 import express from "express";
 import routes from "../routes";
 import {
-  editProfile,
   changePassword,
   userDetail,
+  getEditProfile,
+  postEditProfile,
 } from "../controller/userController";
-import { onlyPrivate } from "../middlewares";
+import { onlyPrivate, uploadAvatar } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile); //  /users/editProfile
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
+
 userRouter.get(routes.changePassword, onlyPrivate, changePassword); // /users/change-password
 userRouter.get(routes.userDetail(), userDetail); // /users/userDetail
 
