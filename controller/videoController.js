e4aa1603b37d5ff394cd9fe3165406653b5cad61
ItsmeLee <arsmagica90@gmail.com>
2020-09-09 22:52:22 +0900

    Fixed the bug
    bug : Can't get inside of videoDetail page when user log out
    
    Cause : LoggedUser.id is undefined without a user being logged in.
    
    How to fix it? : In userDetail and videoDetail templates, Added if-else loggedUser syntax

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
+import { Error } from "mongoose";
 
 export const home = async (req, res) => {
   try {
@@ -67,7 +68,11 @@ export const getEditVideo = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+    if (video.creator !== req.user.id) {
+      throw Error();
+    } else {
+      res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+    }
   } catch (error) {
     console.log(error);
     res.redirect(routes.home);
@@ -93,7 +98,12 @@ export const deleteVideo = async (req, res) => {
     params: { id },
   } = req;
   try {
-    await Video.findOneAndRemove({ _id: id });
+    const video = await Video.findById(id);
+    if (video.creator !== req.user.id) {
+      throw Error();
+    } else {
+      await Video.findOneAndRemove({ _id: id });
+    }
   } catch (error) {
     console.log(error);
   }
