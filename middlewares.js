c99269d0aa8ded06ec2dc7b21f3bee46665ae55f
ItsmeLee <arsmagica90@gmail.com>
2020-09-10 16:19:47 +0900

    Fix the bug : User can't log out
    Fix the bug user can't log out in WeTube

--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,7 @@ const multerAvatar = multer({ dest: "uploads/avatars/" });
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
-  res.locals.loggedUser = req.user || {};
+  res.locals.loggedUser = req.user || null;
   next();
 };
 
